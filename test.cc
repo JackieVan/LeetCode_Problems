a11fdbaf9e6303ff708005485e23a252747d8752
JackieVan <lffan20@fudan.edu.cn>
2022-08-20 20:49:03 +0800

    Add new

--- test.cc
+++ test.cc
@@ -7,47 +7,16 @@
 
 using namespace std;
 
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        std::vector<std::pair<int, int>> seed;
-        for (int i = 0; i != s.size() - 1; ++i)
-        {
-            if (i >=1 && s[i-1] == s[i+1])
-            {
-                seed.emplace_back(i-1, i+1);
-            }
-            if (s[i] == s[i+1]) {
-                seed.emplace_back(i, i+1);
-            }
-        }
-        string ans;
-        int h = 0;
-        int e = 0;
-        for (int i = 0; i != seed.size(); ++i)
-        {
-            int p = seed[i].first;
-            int q = seed[i].second;
-            while (p-1 >= 0 && q+1 < s.size() && s[p - 1] == s[q + 1])
-            {
-                --p;
-                ++q;
-            }
-            if (q-p > h-e)
-            {
-                h = q;
-                e = p;
-            }
-        }
-        return string(s, e, h-e+1);
-    }
+struct A
+{
+    int num;
+    char ch;
 };
+
+
 
 int main()
 {
-    string s = "aaaa";
-    Solution sol;
-    auto vec = sol.longestPalindrome(s);
-    std::cout << vec << "\n";
+    std::cout << sizeof(A) << "\n";
     return 0;
 }
